6ddff5ecc9df86cc2727e39a6598f3da44a20d43
MDaniyalTariq <153484599+MDaniyalTariq@users.noreply.github.com>
2024-10-19 13:54:20 +0500

    Add files via upload

---
+++ output.f90
@@ -0,0 +1,8 @@
+program example
+    REAL :: a, b, c
+    a = 1.0
+    b = 2.0
+    c = a + b
+    print *, c
+    REAL :: x
+end program example
---
+++ sample.f90
@@ -0,0 +1,8 @@
+program example
+    DOUBLE PRECISION :: a, b, c
+    a = 1.0
+    b = 2.0
+    c = a + b
+    print *, c
+    DOUBLE PRECISION :: x
+end program example
